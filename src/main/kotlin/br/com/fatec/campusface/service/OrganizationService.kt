6448492ad6d0f1694f5ae65cfc3451faeff1bfb9
riqalves <henrique12095@gmail.com>
2025-11-26 22:54:06 -0300

    (refactor) feat: novo OrganizationService

--- src/main/kotlin/br/com/fatec/campusface/service/OrganizationService.kt
+++ src/main/kotlin/br/com/fatec/campusface/service/OrganizationService.kt
@@ -4,8 +4,8 @@ import br.com.fatec.campusface.dto.OrganizationCreateDTO
 import br.com.fatec.campusface.dto.OrganizationResponseDTO
 import br.com.fatec.campusface.dto.OrganizationUpdateDTO
 import br.com.fatec.campusface.dto.UserDTO
-import br.com.fatec.campusface.models.Organization
-import br.com.fatec.campusface.models.User
+import br.com.fatec.campusface.models.*
+import br.com.fatec.campusface.repository.OrganizationMemberRepository
 import br.com.fatec.campusface.repository.OrganizationRepository
 import br.com.fatec.campusface.repository.UserRepository
 import org.springframework.stereotype.Service
@@ -14,80 +14,104 @@ import org.springframework.stereotype.Service
 @Service
 class OrganizationService(
     private val organizationRepository: OrganizationRepository,
+    private val organizationMemberRepository: OrganizationMemberRepository,
     private val userRepository: UserRepository,
     private val cloudinaryService: CloudinaryService,
 ) {
 
+
+    fun createOrganization(orgData: OrganizationCreateDTO, creatorAdmin: User): Organization {
+        if (organizationRepository.findByHubCode(orgData.hubCode) != null) {
+            throw IllegalArgumentException("Organizacao já existe, codigo ja em uso")
+        }
+
+        val newOrganization = Organization(
+            name = orgData.name,
+            description = orgData.description,
+            hubCode = orgData.hubCode,
+            adminIds = listOf(creatorAdmin.id),
+        )
+
+        val savedOrg = organizationRepository.save(newOrganization)
+
+        val adminMember = OrganizationMember(
+            organizationId = savedOrg.id,
+            userId = creatorAdmin.id,
+            role = Role.ADMIN,
+            status = MemberStatus.ACTIVE,
+            faceImageId = creatorAdmin.faceImageId, // O admin pode ter foto ou nao
+        )
+
+        organizationMemberRepository.save(adminMember)
+        return savedOrg
+    }
+
+
+    fun getAllOrganizations(): List<OrganizationResponseDTO> {
+        val organizations = organizationRepository.findAll()
+        return organizations.map {org -> hydrateOrganization(org)}
+    }
+
+    fun getOrganizationById(id: String): OrganizationResponseDTO? {
+        val organization = organizationRepository.findById(id)
+        return organization?.let { org -> hydrateOrganization(org) }
+    }
+
+    fun getOrganizationByHubCode(hubCode: String): OrganizationResponseDTO? {
+        val organization = organizationRepository.findByHubCode(hubCode)
+        return organization?.let { org -> hydrateOrganization(org) }
+    }
+
+    fun updateOrganization(id: String, orgData: OrganizationUpdateDTO): OrganizationResponseDTO? {
+        val existingOrg = organizationRepository.findById(id) ?: return null
+
+        val updatedOrg = existingOrg.copy(
+            name = orgData.name,
+            description = orgData.description
+        )
+
+        val savedOrg = organizationRepository.update(id, updatedOrg)
+
+        return savedOrg?.let { org -> hydrateOrganization(org) }
+    }
+
+    fun deleteOrganization(id: String): Boolean {
+        return organizationRepository.delete(id)
+    }
+
+
+
+
     /**
-     * Cria uma nova organização.
-     * @param orgData Os dados da nova organização (nome e descrição).
-     * @param creatorAdmin O usuário ADMIN que está criando a organização.
-     * @return A organização completa que foi salva.
+     * Converte Organization (Entity) -> OrganizationResponseDTO.
+     * Busca os dados completos dos usuários para preencher as listas.
      */
-//    fun createOrganization(orgData: OrganizationCreateDTO, creatorAdmin: User): Organization {
-//        if (orgData.name.isBlank()) {
-//            throw IllegalArgumentException("O nome da organização não pode ser vazio.")
-//        }
-//
-//        // 1. Cria o FaceSet na API do Face++
-//        val newOrganization = Organization(
-//            name = orgData.name,
-//            description = orgData.description,
-//            adminIds = listOf(creatorAdmin.id),
-//        )
-//        return organizationRepository.save(newOrganization)
-//    }
-//
-//    fun deleteOrganization(id: String): Boolean {
-//        // A lógica de deleção não precisa mudar
-//        return organizationRepository.delete(id)
-//    }
-//
-//    fun updateOrganization(id: String, orgData: OrganizationUpdateDTO): OrganizationResponseDTO? {
-//        val existingOrg = organizationRepository.findById(id) ?: return null
-//
-//        val updatedOrg = existingOrg.copy(
-//            name = orgData.name,
-//            description = orgData.description
-//        )
-//
-//        val savedOrg = organizationRepository.update(id, updatedOrg)
-//
-//        return savedOrg?.let { org -> hydrateOrganization(org) }
-//    }
-//
-//
-//    fun getAllOrganizations(): List<OrganizationResponseDTO> {
-//        val organizations = organizationRepository.findAll()
-//        // Mapeia cada organização para seu DTO "hidratado"
-//        return organizations.map { org -> hydrateOrganization(org) }
-//    }
-//
-//    fun getOrganizationById(id: String): OrganizationResponseDTO? {
-//        // Busca a organização com os IDs
-//        val organization = organizationRepository.findById(id)
-//        // Se encontrar, "hidrata" e retorna o DTO; senão, retorna nulo
-//        return organization?.let { org -> hydrateOrganization(org) }
-//    }
-//
-//    /**
-//     * Método auxiliar privado que faz a mágica de "hidratar" os dados.
-//     * Converte um objeto Organization (com IDs) em um OrganizationResponseDTO (com UserDTOs completos).
-//     */
-//    private fun hydrateOrganization(organization: Organization): OrganizationResponseDTO {
-//        // Busca os usuários completos a partir das listas de IDs
-//        val admins = userRepository.findAllByIds(organization.adminIds).map { it.toDTO() }
-//        val validators = userRepository.findAllByIds(organization.validatorIds).map { it.toDTO() }
-//        val members = userRepository.findAllByIds(organization.memberIds).map { it.toDTO() }
-//
-//        return OrganizationResponseDTO(
-//            id = organization.id,
-//            name = organization.name,
-//            description = organization.description,
-//            admins = admins,
-//            validators = validators,
-//            members = members
-//        )
-//    }
+    private fun hydrateOrganization(organization: Organization): OrganizationResponseDTO {
+        val admins = userRepository.findAllByIds(organization.adminIds).map { it.toDTO() }
+        val validators = userRepository.findAllByIds(organization.validatorIds).map { it.toDTO() }
+        val members = userRepository.findAllByIds(organization.memberIds).map { it.toDTO() }
+
+        return OrganizationResponseDTO(
+            id = organization.id,
+            name = organization.name,
+            description = organization.description,
+            hubCode = organization.hubCode,
+            admins = admins,
+            validators = validators,
+            members = members
+        )
+    }
+
+
+    /**
+     * Converte User (Entity) -> UserDTO.
+     * Gera URL assinada temporária para a imagem.
+     */
+    private fun User.toDTO(): UserDTO {
+        val temporaryImageUrl = this.faceImageId?.let { publicId ->
+            cloudinaryService.generateSignedUrl(publicId)
+        }
+        return UserDTO.fromEntity(this, temporaryImageUrl)
+    }
 
 }
