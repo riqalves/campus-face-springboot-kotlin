fa181be6646cba671e098eafbffb1f847dfcd448
riqalves <henrique12095@gmail.com>
2025-11-28 23:23:56 -0300

    docs: adicionando readme

--- src/main/kotlin/br/com/fatec/campusface/dto/ChangeRequestDTO.kt
+++ src/main/kotlin/br/com/fatec/campusface/dto/ChangeRequestDTO.kt
@@ -2,7 +2,16 @@ package br.com.fatec.campusface.dto
 
 
 import br.com.fatec.campusface.models.RequestStatus
+import jakarta.validation.constraints.NotBlank
 import java.time.Instant
+
+data class ChangeRequestCreateDTO(
+    @field:NotBlank(message = "O ID da nova imagem é obrigatório")
+    val newFaceImageId: String,
+
+    @field:NotBlank(message = "O ID da organização é obrigatório")
+    val organizationId: String
+)
 
 data class ChangeRequestResponseDTO(
     val id: String,
